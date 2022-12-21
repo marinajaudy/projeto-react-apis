1c29a5a3dda9ce1fd9c063f66223d29ac741c447
Mjfreedom <mbuzina@gmail.com>
2022-12-20 22:17:10 -0300

    inciando a pg detalhes

--- src/Pages/DetailsPage/Details.js
+++ src/Pages/DetailsPage/Details.js
@@ -1,42 +1,19 @@
 import React, { useContext, useState } from 'react'
 import { Header } from '../../components/Header/Header'
-import { GlobalContext } from '../../contexts/GlobalContext'
-import { ContainerDetails } from './Details.styles'
+import { ContainerDetails, Circle } from './Details.styles'
 import {DetailsCard} from '../../components/DetailsCard/DetailsCard'
-import { useLocation } from 'react-router-dom'
+import pokebolaBackground from '../../assets/pokebolaFundoCard.png'
 
 export const Details = () => {
 
-  const context = useContext(GlobalContext)
-  const { pokemons } = context
-  const location = useLocation()
-
-  // const [pokemonDetails, setPokemonDetails] = useState()
-
-  // const getPokemonByName = async () => {
-  //   try {
-  //     const response = await axios.get(pokemon.url)
-  //     setPokemonDetails(response.data)
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   getPokemonByName()
-  // }, [])
-
   return (
     <>
-    <Header/>
-    <ContainerDetails>
-    <h1>Todos Pokémons</h1>
-          {pokemons.map((pokemon) => {
-            return (<DetailsCard key={pokemon.id} pokemon={pokemon} />)
-          })
-          }
-    </ContainerDetails>
-    
+      <Header />
+      <ContainerDetails>
+      <Circle></Circle>
+        <h1>Detalhes</h1>
+       <DetailsCard/>
+      </ContainerDetails>
     </>
   )
 }
--- src/components/DetailsCard/DetailsCard.js
+++ src/components/DetailsCard/DetailsCard.js
@@ -1,29 +1,25 @@
-import { Button, ChakraProvider, Container } from '@chakra-ui/react'
+import { Box, Button, ChakraProvider, Container, Flex, Image } from '@chakra-ui/react'
 import React, { useEffect, useState, useContext } from 'react'
 import { InfoCard, ImageButton, TypeCard } from './DetailsCard.styled'
 import axios from 'axios'
 import pokebolaBackground from '../../assets/pokebolaFundoCard.png'
 import { typesPokemon } from '../../constants/typesPokemon'
 import { GlobalContext } from '../../contexts/GlobalContext'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import { goToDetailsPage } from '../../Router/coordinator'
 
-export const DetailsCard = (props) => {
-  const { pokemon } = props
+export const DetailsCard = () => {
 
-  const context = useContext(GlobalContext)
-  const location = useLocation()
-  const navigate = useNavigate()
-
-  const {addPokedex} = context
-
-  const [cardPokemon, setCardPokemon] = useState({})
+  // const location = useLocation()
+  // const navigate = useNavigate()
+  const params = useParams()
+  const [cardDetails, setCardDetails] = useState({})
   const [typeApi, setTypeApi] = useState({})
- 
-  const getPokemonByName = async () => {
+
+  const getDetailsByName = async () => {
     try {
-      const response = await axios.get(pokemon.url)
-      setCardPokemon(response.data)
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${params.name}`)
+      setCardDetails(response.data)
       setTypeApi(response.data.types[0].type.name)
       // console.log(typeApi)
     } catch (error) {
@@ -32,65 +28,100 @@ export const DetailsCard = (props) => {
   }
 
   useEffect(() => {
-    getPokemonByName()
+    getDetailsByName()
   }, [])
-    
-  const capitalizeFistLetter = (string) =>{
+
+  const capitalizeFistLetter = (string) => {
     return string && string.charAt(0).toUpperCase() + string.slice(1);
   }
-  
-  // console.log(typeApi)
+
   return (
-    
-      <ChakraProvider>
-        <Container
-            border='2px solid blue'
-            borderRadius='12px'
-            width='83vw'
-            height='76vh'
-            display='flex'
-            justifyContent='space-between'
-            bg={typeApi && typesPokemon[typeApi]?.color}
-            backgroundImage={pokebolaBackground}
-            backgroundRepeat='no-repeat'
-            backgroundPosition='180px'
+
+    <Container
+      border='2px solid blue'
+      borderRadius='38px'
+      maxWidth='1389px'
+      height='663px'
+      display='flex'
+      justifyContent='space-between'
+      bg={typeApi && typesPokemon[typeApi]?.color}
+      backgroundRepeat='no-repeat'
+      backgroundPosition='180px'
+      zIndex='3'
     >
-        <InfoCard>
-                <h3>#{cardPokemon.id}</h3>
-                <h2>{capitalizeFistLetter(cardPokemon.name)}</h2>
-                <TypeCard>
-                  {cardPokemon.types?.map((typePokemon)=>{
-                    return <img src={typesPokemon[typePokemon.type.name].image} alt='img' />
-                  })}
-                </TypeCard>
-                <Button 
-                position='absolute'
-                bottom='5px'
-                fontFamily="'Poppins', sans-serif"
-                fontSize='16px'
-                fontWeight='700'
-                textDecoration='underline'
-                padding='0px'
-                margin='0px'
-                backgroundColor='transparent'
-                onClick={()=>goToDetailsPage(navigate)}
-                >Detalhes</Button>
-              </InfoCard>
-              <ImageButton>
-                <img src={cardPokemon.sprites?.other["official-artwork"].front_default} alt='Imagem Pokémon' />
-                <Button
-                backgroundColor='#FFFFFF'
-                color='#0F0F0F'
-                width='146px'
-                height='38px'
-                position='absolute'
-                bottom='13px'
-                borderRadius='8px'
-                border= '1px dashed rgba(255, 255, 255, 0.47)'
-                onClick={()=>addPokedex(cardPokemon)}
-                >Capturar!</Button>
-              </ImageButton>
-      </Container>
-        </ChakraProvider>
+      <Flex
+        width='50%'
+        justifyContent='center'
+        alignContent='center'
+        padding='24px 0px'
+      >
+        <Box
+          display='flex'
+          flexDirection='column'
+          justifyContent='space-between'
+        >
+          <Image
+            borderRadius='8px'
+            width='282px'  
+            height='282px'
+            backgroundColor='white'
+            src={cardDetails.sprites?.["front_default"]} alt='Imagem Pokemon de Costas' />
+            <Image
+            borderRadius='8px'
+            width='282px'
+            height='282px'
+            backgroundColor='white'
+            src={cardDetails.sprites?.["back_default"]} alt='Imagem Pokemon de Frente' />
+        </Box>
+        <Box
+          backgroundColor='white'
+          width='343px'
+          height='100%'
+          borderRadius='12px'
+          color='black'
+        >
+          <h1>AQUI VAI O CARD CHAKRA COM MEDIDOR</h1>
+        </Box>
+      </Flex>
+      <InfoCard>
+        <Flex
+        border='2px solid red'
+        marginTop='-20px'
+        width='100%'
+        height='22vh'
+        gap='16px'
+        >
+        <h3>#{cardDetails.id}</h3>
+        <h2>{capitalizeFistLetter(cardDetails.name)}</h2>
+        <TypeCard>
+          {cardDetails.types?.map((typePokemon) => {
+            return <img src={typesPokemon[typePokemon.type.name].image} alt='img' />
+          })}
+        </TypeCard>
+        </Flex>
+        <Image
+        position= 'absolute'
+        width= '270px'
+        height= '270px'
+        left= '320px'
+        top= '-160px'
+        src={cardDetails.sprites?.other["official-artwork"].front_default} alt='Imagem Pokémon' />
+        <Flex
+        border='2px solid green'
+        width='100%'
+        display='flex'
+        justifyContent='flex-start'
+        >
+        <Box
+            backgroundColor='white'
+            width='292px'
+            height='453px'
+            color='black'
+          >
+            <h1> AQUI VEM INFO A MAIS DO POKEMON</h1>
+          </Box>
+        </Flex>
+      </InfoCard>
+    </Container>
   )
 }
